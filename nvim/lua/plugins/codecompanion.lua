747517094901bb5060fb963913a656bce9c3d675
Johan Björklund <johan.bjorklund@proton.me>
2025-06-25 09:19:41 +0000

    feat(nvim): use CodeCompanion over CopilotChat

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,71 @@
+return {
+  'olimorris/codecompanion.nvim',
+  opts = {
+    display = {
+      action_palette = {
+        position = 'bottom',
+        width = 95,
+        hight = 10,
+        provider = 'telescope',
+        opts = {
+          show_default_actions = true,
+          show_default_prompt_library = true
+        },
+      },
+      chat = {
+        window = {
+        position = 'right',
+          width = 0.3,
+        },
+      },
+    },
+    extensions = {
+      mcphub = {
+        callback = 'mcphub.extensions.codecompanion',
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_results_in_chat = true,
+        },  
+      },
+    },
+  },
+  keys = {
+    {'<leader>cp', '<cmd>CodeCompanion<cr>', desc = 'AI [P]rompt'},
+    {'<leader>cs', '<cmd>CodeCompanionChat<cr>', desc = 'AI [S]tart'},
+    {
+      '<leader>cc',
+      function()
+        require('codecompanion').prompt('commit')
+      end,
+      desc = 'AI [C]ommit'
+    },
+    {
+      '<leader>ce',
+      function()
+        require('codecompanion').prompt('explain')
+      end,
+      desc = 'AI [E]xplain'
+    },
+    {
+      '<leader>ct',
+      function()
+        require('codecompanion').prompt('tests')
+      end,
+      desc = 'AI [T]ests'
+    },
+    {
+      '<leader>cf',
+      function()
+        require('codecompanion').prompt('fix')
+      end,
+      desc = 'AI [F]ix'
+    },
+  },
+  dependencies = {
+    {'nvim-lua/plenary.nvim'},
+    {'nvim-treesitter/nvim-treesitter'},
+    {'ravitemer/mcphub.nvim'},
+    {'echasnovski/mini.diff', opts = {}},
+  }
+}
